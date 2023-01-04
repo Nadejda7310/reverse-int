3e93b0bb483b2245f78b7da3acc15a03617c72ee
Nadejda <bloger.n.m@gmail.com>
2023-01-04 10:50:13 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
+  let strN = n.toString()
+  let result = ''
   
+  for (let i = 0; strN.length > i; i++){
+    if (strN[i] === '-'){
+        result = `${result}`
+    }
+    else {result = `${strN[i]}${result}`}
+  }
+
+return result;
 }
